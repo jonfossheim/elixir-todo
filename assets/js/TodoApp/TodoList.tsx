4ab940604198c822e041dfb5ae4c3eb122b961f1
jonfossheim <fossheimjoneven@gmail.com>
2020-08-17 15:59:27 +0200

    added query to frontend

---
+++ assets/js/TodoApp/TodoList.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+interface TodoItem {
+    id: number | string;
+    content: string;
+    isCompleted: boolean;
+}
+
+interface TodoItemsQueryResult {
+    todoItems: TodoItem[];
+}
+
+const TodoList = () => {
+    const { data, loading } = useQuery<TodoItemsQueryResult>(gql`
+    {
+      todoItems {
+        id
+        content
+        isCompleted
+      }
+    }
+  `);
+
+    return (
+        <div className="todo_list">
+            <h3 className="todo_list__header">Todo Items</h3>
+            <ul className="todo_list__list">
+                {data?.todoItems
+                    ? data.todoItems.map((item) => (
+                        <li
+                            key={item.id}
+                            className={
+                                item.isCompleted
+                                    ? "todo_list__item todo_list__item--completed"
+                                    : "todo_list__item"
+                            }
+                        >
+                            {item.content}
+                        </li>
+                    ))
+                    : null}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
--- assets/js/TodoApp/index.tsx
+++ assets/js/TodoApp/index.tsx
@@ -1,16 +1,16 @@
 import React from "react"
+import client from "./client"
+import { ApolloProvider } from "@apollo/react-hooks"
+import TodoList from "./TodoList"
 
 const TodoApp = () => {
     return (
-        <>
-            <h1>test</h1>
-            <div className="container mx-auto bg-green-400 rounded-lg p-6">
-                test
-                test
-                test
-            </div>
-            <p>Hello</p>
-        </>
+        <ApolloProvider client={client}>
+            <div className="wrapper bg-gray-100">
+                <h1 className="text-6xl">What <span className="italic font - semibold">ToDo</span>?</h1>
+                <TodoList />
+            </div >
+        </ApolloProvider>
     )
 
 }
